c9a77e3ee8399411eb578d0d4dfc98043a08394e
heri16 <heri16@gmail.com>
2016-02-15 17:12:35 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-if [ -z "$FPM_PORT_9000" ]; then
+if [ -n "${FPM_PORT_9000_TCP_PORT+1}" ]; then
     sed -i -e"s/^\#\(\s*\(FastCGIserver\|FastCGIid\|ConnectTo\).*\)/\1/" -e"s/^\#\(\s*Extension.*\)/\1\n\}/" /etc/hiawatha/hiawatha.conf
-    sed -i -e"s/\(ConnectTo\s=\).*$/\1 fpm:$FPM_PORT_9000_TCP_PORT/" /etc/hiawatha/hiawatha.conf
+    sed -i -e"s/\(ConnectTo\s=\).*$/\1 $FPM_PORT_9000_TCP_ADDR:$FPM_PORT_9000_TCP_PORT/" /etc/hiawatha/hiawatha.conf
 fi
 
 exec /usr/sbin/hiawatha -d
