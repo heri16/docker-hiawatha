6941bee78a72defa3d9da98992de8d3eef59608c
heri16 <heri16@github.com>
2016-09-12 23:02:58 -0400

    Resolve PHP_HOST to IP address (required by hiawatha)

--- run.sh
+++ run.sh
@@ -2,9 +2,10 @@
 set -e
 
 if [ -n "${PHP_HOST+1}" ] && [ -n "${PHP_FPM_PORT+1}" ]; then
+    PHP_HOST_IP="$(getent hosts ${PHP_HOST} | awk '{ print $1 }')"
     sed -i -e"s/\(FastCGIid\s=\).*$/\1 PHP/" /etc/hiawatha/hiawatha.conf
     sed -i -e"s/\(Extension\s=\).*$/\1 php/" /etc/hiawatha/hiawatha.conf
-    sed -i -e"s/\(ConnectTo\s=\).*$/\1 ${PHP_HOST}:${PHP_PORT}/" /etc/hiawatha/hiawatha.conf
+    sed -i -e"s/\(ConnectTo\s=\).*$/\1 ${PHP_HOST_IP}:${PHP_FPM_PORT}/" /etc/hiawatha/hiawatha.conf
     sed -i -e"s/^\#\(\s*\(FastCGIserver\|FastCGIid\|ConnectTo\).*\)/\1/" -e"s/^\#\(\s*Extension.*\)/\1\n\}/" /etc/hiawatha/hiawatha.conf
 elif [ -n "${PHP_PORT_9000_TCP_ADDR+1}" ] && [ -n "${PHP_PORT_9000_TCP_PORT+1}" ]; then
     sed -i -e"s/\(FastCGIid\s=\).*$/\1 PHP/" /etc/hiawatha/hiawatha.conf
